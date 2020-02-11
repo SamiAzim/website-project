57ed638db4d6ff910a44514a0a1da830af8ecd9d
Sami Abdelazim <samiabdelazim@Samis-MacBook-Pro.local>
2020-02-10 22:38:56 -0500

    finished blog

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def date_special(self):
+        return self.pub_date.strftime('%b %e %Y')
